fcdfd7871882495bb34166f7c01e993ed7c01f5f
mbprtpmnr <mbprtpmnr@pm.me>
2021-09-29 10:43:46 +0300

    refactor!: added some firefox settings

--- home/modules/programs.nix
+++ home/modules/programs.nix
@@ -2,6 +2,7 @@
 
 let
   home = config.home.homeDirectory;
+  font = "Iosevka SS08";
 in
 
 {
@@ -16,6 +17,37 @@ in
     profiles = {
       mbprtpmnr = {
         isDefault = true;
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "font.name.monospace.x-western" = font;
+          "font.name.sans-serif.x-western" = font;
+          "font.name.serif.x-western" = font;
+          "browser.safebrowsing.downloads.remote.block_dangerous" = false;
+          "browser.safebrowsing.downloads.remote.block_dangerous_host" = false;
+          "browser.safebrowsing.downloads.remote.block_potentially_unwanted" = false;
+          "browser.safebrowsing.downloads.remote.block_uncommon" = false;
+          "browser.safebrowsing.downloads.remote.url" = false;
+          "browser.safebrowsing.downloads.remote.enabled" = false;
+          "browser.safebrowsing.downloads.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+          "datareporting.healthreport.service.enabled" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "experiments.supported" = false;
+          "experiments.enabled" = false;
+          "experiments.manifest.uri" = "";
+          "browser.discovery.enabled" = false;
+          "extensions.shield-recipe-client.enabled" = false;
+          "app.shield.optoutstudies.enabled" = false;
+          "loop.logDomains" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "network.cookie.cookieBehavior" = 1;
+          "browser.shell.checkDefaultBrowser" = false;
+          "browser.download.dir" = "${home}/Downloads";
+          "browser.download.folderList" = 2;
+        };
       };
     };
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
